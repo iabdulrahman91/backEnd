2d1f89fca723b1fb26230d5a4de1cb25d51d3918
iabdulrahman91 <aalana10@mail.depaul.edu>
2019-07-23 21:07:35 -0500

    User auth system for api is completed; you may need Listing to seed user db

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(User::class, function (Faker $faker) {
+
+    return [
+        //
+        'fname' => $faker->firstName,
+        'lname' => $faker->lastName,
+        'email' => $faker->email,
+        'phone' => $faker->numerify("05########"),
+        'password' => bcrypt('aassdd'),
+    ];
+});
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $users = factory(App\User::class, 30)
+            ->create()
+            ->each(function ($user) {
+                foreach (range(1,rand(2,5)) as $i){
+                    $user->addListing(factory(App\Listing::class)->make());
+                }
+
+            });
+    }
+}
