c16658a41207973a4382323eb6ad4f4bda1a3902
iabdulrahman91 <aalana10@mail.depaul.edu>
2019-07-28 17:06:45 -0500

    index: is updated to show only active listings.
    show: start using IDs to prevent not found error binding is bling LOL.
    
    store: user $request , having $input was bad idea (Casting issue). So I access the attributes directly from the $request.
    store: user int for days status instead of boolean.
    
    update: finish the updating days feature. listing owner can update the days of a listing with some restrictions.
    update: Only days
    
    destroy: owner can deactivate a listing, and it won't be showing in Index method. However, it still can be accessed by show method by anyone.

--- app/Http/Controllers/Api/ListingController.php
+++ app/Http/Controllers/Api/ListingController.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
+use Exception;
 use App\Listing;
 use App\Rules\Days;
 use App\Rules\Item;
 use App\Rules\Location;
 use Carbon\Carbon;
-use function GuzzleHttp\describe_type;
 use Validator;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -34,7 +33,7 @@ class ListingController extends Controller
     public function index()
     {
         //
-        $listings = Listing::paginate(10);
+        $listings = Listing::where('active',true)->paginate(30);
         return ListingResource::collection($listings);
     }
 
@@ -45,10 +44,17 @@ class ListingController extends Controller
      * @param \App\Listing $listing
      * @return \Illuminate\Http\Response
      */
-    public function show(Listing $listing)
+    public function show(int $id)
     {
         //
+        $listing = Listing::find($id);
 
+        // make user the requester it the owner
+        if ($listing == null) {
+            return response()
+                ->json(['message' => 'Not Found.'])
+                ->setStatusCode(404);
+        }
         return new ListingResource($listing);
     }
 
@@ -61,13 +67,13 @@ class ListingController extends Controller
     public function store(Request $request)
     {
         // get data from the request
+
         $user = Auth::user();
-        $inputs = $request->all(['location', 'item', 'days', 'price']);
 
 
 
         // see Rules/ for more info about the validation process
-        $validator = Validator::make($inputs, [
+        $validator = Validator::make($request->all(), [
             'location' => ['Required', new Location()],
             'item' => ['Required', new Item()],
             'days' => ['Required', new Days()],
@@ -82,17 +88,16 @@ class ListingController extends Controller
         }
 
 
-
         // prepare listing data
-        $location = json_encode($inputs['location']);
-        $item = json_encode($inputs['item']);
-        $price = ($inputs['price']);
+        $location = json_encode($request['location']);
+        $item = json_encode($request['item']);
+        $price = ($request['price']);
 
 
         // format the dates to day-month-year : 24-12-1991
         $days = array();
-        foreach ($inputs['days'] as $d) {
-            $days[Carbon::parse($d)->format('d-m-Y')] = true;
+        foreach ($request['days'] as $d) {
+            $days[Carbon::parse($d)->format('d-m-Y')] = 1;
         }
         $days = json_encode($days);
 
@@ -101,7 +106,8 @@ class ListingController extends Controller
             'location' => $location,
             'item' => $item,
             'price' => $price,
-            'days' => $days
+            'days' => $days,
+
         ]);
 
         // pass the listing object to be created by the user
@@ -119,9 +125,83 @@ class ListingController extends Controller
      * @param \App\Listing $listing
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Listing $listing)
+    public function update(Request $request, int $id)
     {
-        // TODO
+        // verify user's authority, and listing existence
+        $user = Auth::user();
+        $listing = Listing::find($id);
+
+        // make user the requester it the owner
+        if ($listing == null || $listing->user->id != $user->id) {
+            return response()
+                ->json(['message' => 'Unauthorized.'])
+                ->setStatusCode(401);
+        }
+
+        // validate new data
+        $validator = Validator::make($request->all(), [
+            'days' => ['sometimes', 'Required', new Days()],
+        ]);
+
+        if ($validator->fails()) {
+            return response()
+                ->json(['error' => $validator->errors()])
+                ->setStatusCode(400);
+        }
+
+        // if no updated attributes is submitted
+        if (!$request->has(['days']) || empty($request['days'])) {
+            return response()
+                ->json(['message' => 'No update made']);
+        }
+
+
+        // update the listing
+        /*
+         * Days status:
+         * 0 : inactive / pending
+         * 1 : active and can be requested.
+         * 2 : rented.
+         * 3 : user delete.
+         * 4 : management delete.
+         */
+        $oldDays = json_decode($listing->days);
+        $newDays = array();
+
+
+        // deactivate all active days. Leave other dates if not active (1)
+        // only set the value for active days (1) or whatever default value for new listing days
+        // default value now is 1 as the mgmt allow users to directly publish their listings. However, in the future we might have
+        // mgmt approving new list. In this case we need to consider pending days candidate for update to 3.
+        // make sure no date impact current transaction
+        foreach ($oldDays as $d => $s) {
+            $newDays[$d] = ($s = 1)? 3 : $s;
+        }
+
+
+        // format the dates to day-month-year : 24-12-1991
+        // and add new days
+        // just add new dates or update the one deleted by user (3)
+        foreach ($request['days'] as $d) {
+            $day = Carbon::parse($d)->format('d-m-Y');
+
+            try {
+                $newDays[$day] = ($newDays[$day] == 3 )? 1 : $newDays[$day];
+            } catch (Exception $error){
+                $newDays[$day] = 1;
+            }
+
+        }
+
+        // assign new days to the listing
+        $listing->days = json_encode($newDays);
+
+        // pass the listing object to be created by the user
+        $l = $user->listings()->save($listing);
+
+
+        // confirm success
+        return new ListingResource($l);
     }
 
     /**
@@ -130,8 +210,28 @@ class ListingController extends Controller
      * @param \App\Listing $listing
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Listing $listing)
+    public function destroy(int $id)
     {
-        // TODO
+        $user = Auth::user();
+        $listing = Listing::find($id);
+
+        // make user the requester it the owner
+        if ($listing == null || $listing->user->id != $user->id) {
+            return response()
+                ->json(['message' => 'Unauthorized.'])
+                ->setStatusCode(401);
+        }
+
+        // change the listing status to false
+        $listing->active = false;
+
+        // pass the listing object to be created by the user
+        $l = $user->listings()->save($listing);
+
+
+        // confirm success
+        return new ListingResource($l);
+
+
     }
 }
